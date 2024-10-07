f2924955f468950dcc78afe0e90382a2ccc199c3
coach4u <34412145+coach4u@users.noreply.github.com>
2024-10-07 17:18:41 +0530

    Update listener.tf

--- webapp/listener.tf
+++ webapp/listener.tf
@@ -1,12 +1,12 @@
 resource "aws_lb_listener" "http_listener" {
-  load_balancer_arn = aws_lb.mytest_lb.arn
+  load_balancer_arn = aws_lb.test_lb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.mytest_tg.arn
+    target_group_arn = aws_lb_target_group.test_tg.arn
   }
 
-  depends_on = [aws_lb_target_group.mytest_tg]
+  depends_on = [aws_lb_target_group.test_tg]
 }
