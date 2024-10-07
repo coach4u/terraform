68dc85f7c99edc671c6388fa7267d4bd256fe3a6
coach4u <34412145+coach4u@users.noreply.github.com>
2024-10-07 17:16:09 +0530

    Update route.tf

--- webapp/route.tf
+++ webapp/route.tf
@@ -1,23 +1,23 @@
-resource "aws_route_table" "mytest_rt" {
-  vpc_id = aws_vpc.mytest_vpc.id
+resource "aws_route_table" "test_rt" {
+  vpc_id = aws_vpc.test_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.mytest_igw.id
+    gateway_id = aws_internet_gateway.test_igw.id
   }
 
   tags = {
-    Name = "mytest-rt"
+    Name = "test-rt"
   }
 }
 
 # Associate subnets with route table
-resource "aws_route_table_association" "mytest_rt_assoc_1" {
-  subnet_id      = aws_subnet.mytest_subnet_1.id
-  route_table_id = aws_route_table.mytest_rt.id
+resource "aws_route_table_association" "test_rt_assoc_1" {
+  subnet_id      = aws_subnet.test_subnet_1.id
+  route_table_id = aws_route_table.test_rt.id
 }
 
-resource "aws_route_table_association" "mytest_rt_assoc_2" {
-  subnet_id      = aws_subnet.mytest_subnet_2.id
-  route_table_id = aws_route_table.mytest_rt.id
+resource "aws_route_table_association" "test_rt_assoc_2" {
+  subnet_id      = aws_subnet.test_subnet_2.id
+  route_table_id = aws_route_table.test_rt.id
 }
