fe0fa9b5528f6fba281982494e63d519437953d9
coach4u <34412145+coach4u@users.noreply.github.com>
2024-10-07 17:14:42 +0530

    Update subnet.tf

--- webapp/subnet.tf
+++ webapp/subnet.tf
@@ -1,21 +1,21 @@
 #Creating subnets
 
-resource "aws_subnet" "mytest_subnet_1" {
-  vpc_id     = aws_vpc.mytest_vpc.id
+resource "aws_subnet" "test_subnet_1" {
+  vpc_id     = aws_vpc.test_vpc.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "mytest-subnet-1"
+    Name = "test-subnet-1"
   }
 }
 
-resource "aws_subnet" "mytest_subnet_2" {
-  vpc_id     = aws_vpc.mytest_vpc.id
+resource "aws_subnet" "test_subnet_2" {
+  vpc_id     = aws_vpc.test_vpc.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1c"
   tags = {
-    Name = "mytest-subnet-2"
+    Name = "test-subnet-2"
   }
 }
 
