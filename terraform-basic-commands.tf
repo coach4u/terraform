4369f41b0ce1242b8bc6d6852d0e1e0af1415fd8
coach4u <34412145+coach4u@users.noreply.github.com>
2024-10-17 12:20:39 +0530

    terraform-basic-commands.tf

---
+++ terraform-basic-commands.tf
@@ -0,0 +1,13 @@
+Terraform commands to get started:
+
+
+terraform init     (initializes the working directory by downloading provider plugins and configuring the backend. It prepares Terraform for future commands like plan and apply.)
+
+terraform validate (checks the syntax and structure of your Terraform configuration files.Ensures they are correct and ready for deployment.used after you initialize your working directory with terraform init.) 
+
+terraform plan     (preview changes as to what it is going to create / update / destroy.without making any actual changes)
+
+terraform apply    (Executes the changes defined in your Terraform configuration, creating, modifying, or destroying resources as shown in the terraform plan)
+
+terraform destroy   (Removes all the resources defined in your Terraform configuration, deleting the infrastructure that was previously created.)
+
