63b2bfd52240cfbdabba7a8a95d8d4ced27e634c
coach4u <34412145+coach4u@users.noreply.github.com>
2025-04-30 19:27:56 +0530

    provider.tf

--- azure/provider.tf
+++ azure/provider.tf
@@ -10,7 +10,7 @@ terraform {
 
 provider "azurerm" {
     features{}
-    subscription_id="9734ed68-621d-47ed-babd-269110dbacb1"
+    subscription_id=""
     resource_provider_registrations = "none"
 
 }
