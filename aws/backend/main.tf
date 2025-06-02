13505554f6e3a7fbbc829758bb99853b146dcc61
coach4u <34412145+coach4u@users.noreply.github.com>
2025-06-02 12:20:40 +0530

    Create main.tf

---
+++ aws/backend/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "sample-terraform-eks-state-bucket"
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_dynamodb_table" "dynamodb-table" {
+  name           = "terraform-eks-state-locks"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
